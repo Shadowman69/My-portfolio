df88f4e015c1012342d6eb5fdbf51789fe148452
Shadowman69 <vasyl.v.bereza@gmail.com>
2018-05-12 18:21:35 +0300

    Made some fixed as for menu
    Fixed hamburger menu script, added menu items animation

--- Blueasy PSD Template/js/scripts.js
+++ Blueasy PSD Template/js/scripts.js
@@ -5,17 +5,19 @@ $(document).ready(function() {
             $menu = $('nav ul.menu');
 
         $trigger.click( function() {
-            $menu.slideToggle(); 
+            $menu.slideToggle();
+            /*$menu.css({"position": "absolute", "width": "100%"});
+            $menu.css({"background-color": "#475159", "z-index": "1"});*/
         });
 
         $(window).resize(function() {
-            if((window).width() > 550) {
+            if((window).width > '550') {
                 $menu.removeAtrr('style');
             };
         });
     };
 
+    blueasyMenu();
     
 });
 
-blueasyMenu();
